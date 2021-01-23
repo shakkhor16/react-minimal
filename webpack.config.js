8ed38587cef21ccc7175e0460f158855edd774e2
Md. Ruhul Quddus <shakkhor16@gmail.com>
2021-01-23 23:45:34 +0600

    In the beginning the Universe was created. This has made a lot of people very angry and has been widely regarded as a bad move.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.[contenthash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+            favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
+        }),
+    ],
+};
